3b81d623986a94796d34966d646901bb8ade928b
naren38 <narenreddy38@gmail.com>
2024-12-07 00:19:42 +1100

    L

--- week_4/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const mongoose = require('./database'); // Import database connection
-const app = express();const PORT = process.env.PORT || 5000;
- app.use(express.json()); // For parsing JSON bodies 
- app.get("/", (req, res) => {     
-res.send("Welcome to the backend!"); }); // Start the server
-app.listen(PORT, () => {     
-console.log(`Server is running on port ${PORT}`);
-});
